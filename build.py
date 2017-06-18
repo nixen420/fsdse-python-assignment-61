d89db40b7a2142abb6f519e8a943fbb14cedecbd
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 16:29:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    x = y = 0
+    for i in list_of_nums:
+        if i%2==0:
+            y+=1
+        else:
+            x+=1
+    return {'ODD': x, 'EVEN': y}
