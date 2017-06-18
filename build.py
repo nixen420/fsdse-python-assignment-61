f3c1d2faf1f7ffdf838cbcc2aaa4055f00f2d3f8
nixen420 <nikhil.singh4008@gmail.com>
2017-06-18 16:30:21 +0000

    Done

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+list_of_nums=[1, 2, 3, 5, 8, 9]
 def solution(list_of_nums):
     x = y = 0
     for i in list_of_nums:
@@ -6,3 +7,4 @@ def solution(list_of_nums):
         else:
             x+=1
     return {'ODD': x, 'EVEN': y}
+solution(list_of_nums)
